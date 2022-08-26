39ad259b9850c189191100e8e14ee22f4c410938
Evgeny <iusko.jenya@yandex.ru>
2022-08-26 18:57:09 +0300

    SnowBall docs
    Добавлена документация типов системы SnowBall.

--- SnowEngine/Source/Util/Input/Input.h
+++ SnowEngine/Source/Util/Input/Input.h
@@ -9,10 +9,23 @@
 //\\         /\\_ \\//\  ___ /\\/ __ /\\  \\/  \//  //  // _//\ __ \//\\  \\  \\//\\//\\//\\//\\//\
 //\_         _\\/__ //\  /\\  \\  \\  \\  \\/  \//  //  __ \//  //  //\\  \\  \\/| SnowEngine |\//\
 //\\__     __/\\//\  /\  /\\  \\_ \\ _\\_ \\ __ // _//  // _//_ //  //\\  \\  \\//| input      |//\
-//\\//_____\//\\____//\__/\\__\\/____/\\/____/\____\//_____\//\_______\\__\\__\\//\| system     |/\
+//\\//_____\//\\____//\__/\\__\\/____/\\/____/\____\//_____\//\____,__\\__\\__\\//\| system     |/\
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\
 
 #pragma once
+
+/**
+ *	\file
+ *	\~english
+ *	\brief The file with `Input` class
+ *
+ *	This file contains the definition of the `Input` class of the SnowBall system.
+ *
+ *	\~russian
+ *	\brief Файл с классом `Input`
+ *
+ *	Этот файл содержит определение класса `Input` системы SnowBall.
+ */
 
 #include "../../Object.h"
 
@@ -33,6 +46,19 @@ class Point2;
 class Vector2;
 class CameraComponent;
 
+/**
+ *	\~english
+ *	\brief The class of the SnowBall input manager
+ *
+ *	This class is used to handle input. It is singleton, only one input manager may exist. You can
+ *	get it using `get_instance` static method.
+ *
+ *	\~russian
+ *	\brief Класс диспетчера ввода SnowBall
+ *
+ *	Этот класс используется для обработки ввода. Он является одиночкой: может существовать только
+ *	один диспетчер ввода. Вы можете получить его, используя статический метод `get_instance`.
+ */
 class Input : public Object
 {
 	friend class Game;
@@ -40,42 +66,284 @@ class Input : public Object
 public:
 			/* SINGLETON */
 
+	/**
+	 *	\~english
+	 *	\brief The only instance of the input manager
+	 *	
+	 *	Allows to get the only instance of the input manager.
+	 *	\return The input manager.
+	 *	
+	 *	\~russian
+	 *	\brief Единственный экземпляр диспетчера ввода
+	 *	
+	 *	Позволяет получить единственный экземпляр диспетчера ввода.
+	 *	\return Диспетчер ввода.
+	 */
 	static Input& get_instance();
 
 			/* METHODS FROM Object */
 
+	/**
+	 *	\~english
+	 *	\brief Converts the input manager to string
+	 *	
+	 *	Creates the string `Input object`.
+	 *	\return Created string.
+	 *	
+	 *	\~russian
+	 *	\brief Конвертирует диспетчер ввода в строку
+	 *	
+	 *	Создаёт строку `Input object`.
+	 *	\return Созданная строка.
+	 */
 	virtual String to_string() const override;
-	
+
+	/**
+	 *	\~english
+	 *	\brief Hash code of the input manager
+	 *
+	 *	Hash code is an integer number. Hash codes of two equal object are equal, but two different
+	 *	objects can also have the same hash codes. The hash code of the input manager is always
+	 *	zero.
+	 *	\return Hash code of the object.
+	 *
+	 *	\~russian
+	 *	\brief Хеш-код диспетчера ввода
+	 *
+	 *	Хеш-код — это целое число. Хеш-коды двух равных объектов равны, но два различных объекта
+	 *	также могут иметь одинаковые хеш-коды. Хеш-код диспетчера ввода — всегда ноль.
+	 *	\return Хеш-код объекта.
+	 */
 	virtual int hash_code() const noexcept override;
 
 			/* METHODS */
 
+	/**
+	 *	\~english
+	 *	\brief Checks whether the key is pressed
+	 *	
+	 *	Checks whether the passed keyboard key is now pressed.
+	 *	\param key The key to check.
+	 *	\return `true` if the key is pressed, `false` otherwise.
+	 *	
+	 *	\~russian
+	 *	\brief Проверяет, нажата ли клавиша
+	 *	
+	 *	Проверяет, нажата ли в данный момент переданная клавиша на клавиатуре.
+	 *	\param key Клавиша для проверки.
+	 *	\return `true`, если клавиша нажата, иначе `false`.
+	 */
 	bool is_key_pressed(EKey key);
 
+	/**
+	 *	\~english
+	 *	\brief Which system keys are pressed
+	 *	
+	 *	Detects system keys which are pressed at the moment.
+	 *	\return The information about pressed system keys.
+	 *	
+	 *	\~russian
+	 *	\brief Какие системные клавиши нажаты
+	 *	
+	 *	Определяет системные клавиши, которые нажаты в данный момент.
+	 *	\return Информация о нажатых системных клавишах.
+	 */
 	SystemKeys get_system_keys();
 
+	/**
+	 *	\~english
+	 *	\brief The mouse position on the screen
+	 *	
+	 *	Allows to get the mouse position on the screen in desktop coordinates.
+	 *	\return The mouse position on the screen.
+	 *	
+	 *	\~russian
+	 *	\brief Позиция курсора мыши на экране
+	 *	
+	 *	Позволяет получить позицию курсора мыши в координатах экрана.
+	 *	\return Позиция курсора мыши на экране.
+	 */
 	Point2 get_screen_mouse_position();
+	
+	/**
+	 *	\~english
+	 *	\brief The mouse position in the game window
+	 *	
+	 *	Allows to get the mouse position in the game window.
+	 *	\return The mouse position in the window, zero point if the window hasn't been created yet.
+	 *	
+	 *	\~russian
+	 *	\brief Позиция курсора мыши в игровом окне
+	 *	
+	 *	Позволяет получить позицию курсора мыши в игровом окне.
+	 *	\return Позиция курсора мыши в окне; нулевая точка, если окно ещё не было создано.
+	 */
 	Point2 get_window_mouse_position();
+
+	/**
+	 *	\~english
+	 *	\brief The mouse position in the level
+	 *	
+	 *	Allows to get the mouse position in the level coordinates.
+	 *	\param relative_to The camera component whose viewport will be used.
+	 *	\return The mouse position on the viewport of the passed camera in the level coordinates;
+	 *	zero vector if the window hasn't been created yet.
+	 *	
+	 *	\~russian
+	 *	\brief Позиция курсора мыши на уровне
+	 *	
+	 *	Позволяет получить позицию курсора мыши в координатах уровня.
+	 *	\param relative_to Компонент-камера, чья область просмотра будет использоваться.
+	 *	\return Позиция курсора мыши на области просмотра переданной камеры в координатах уровня;
+	 *	нулевой вектор, если окно ещё не было создано.
+	 */
 	Vector2 get_level_mouse_position(const CameraComponent& relative_to);
+
+	/**
+	 *	\~english
+	 *	\brief Sets the mouse position on the screen
+	 *
+	 *	Allows to change the mouse position on the screen in desktop coordinates.
+	 *	\param position The new mouse position on the screen.
+	 *
+	 *	\~russian
+	 *	\brief Устанавливает позицию курсора мыши на экране
+	 *
+	 *	Позволяет изменить позицию курсора мыши в координатах экрана.
+	 *	\param position Новая позиция курсора мыши на экране.
+	 */
 	void set_screen_mouse_position(const Point2& position);
+
+	/**
+	 *	\~english
+	 *	\brief Sets the mouse position in the game window
+	 *
+	 *	Allows to change the mouse position in the game window. Does nothing if the window hasn't
+	 *	been created yet.
+	 *	\param position The new mouse position in the window coordinates.
+	 *
+	 *	\~russian
+	 *	\brief Устанавливает позицию курсора мыши в игровом окне
+	 *
+	 *	Позволяет изменить позицию курсора мыши в игровом окне. Если окно ещё не было создано, не
+	 *	делает ничего.
+	 *	\param position Новая позиция курсора мыши в координатах окна.
+	 */
 	void set_window_mouse_position(const Point2& position);
+
+	/**
+	 *	\~english
+	 *	\brief Sets the mouse position in the level
+	 *
+	 *	Allows to change the mouse position in the level coordinates. Does nothing if the window
+	 *	hasn't been created yet.
+	 *	\param position The new mouse position on the viewport of the passed camera in the level
+	 *	coordinates.
+	 *	\param relative_to The camera component whose viewport will be used.
+	 *
+	 *	\~russian
+	 *	\brief Устанавливает позицию курсора мыши на уровне
+	 *
+	 *	Позволяет изменить позицию курсора мыши в координатах уровня. Если окно ещё не было
+	 *	создано, не делает ничего.
+	 *	\param position Новая позиция курсора мыши на области просмотра переданной камеры в
+	 *	координатах уровня.
+	 *	\param relative_to Компонент-камера, чья область просмотра будет использоваться.
+	 */
 	void set_level_mouse_position(const Vector2& position, const CameraComponent& relative_to);
 
-	EventBinder<SystemKeys>& on_pressed(EKey key);
-	EventBinder<SystemKeys>& on_released(EKey key);
+			/* EVENTS */
+
+	/**
+	 *	\~english
+	 *	\brief The key is pressed
+	 *	
+	 *	This function allows to get the event that is called when the key is pressed.
+	 *	
+	 *	Event parameters:
+	 *	- `SystemKeys pressed_system`: The information about system keys that are pressed now.
+	 *	\param key The key.
+	 *	\return The event binder.
+	 *	
+	 *	\~return
+	 *	\brief Клавиша нажата
+	 *	
+	 *	Эта функция позволяет получить событие, вызываемое, когда клавиша нажата.
+	 *	
+	 *	Параметры события:
+	 *	- `SystemKeys pressed_system`: Информация о системных клавишах, нажатых в данный момент.
+	 *	\param key Клавиша.
+	 *	\return «Привязыватель» события.
+	 */
+	EventBinder<SystemKeys /*pressed_system*/>& on_pressed(EKey key);
+
+	/**
+	 *	\~english
+	 *	\brief The key is released
+	 *
+	 *	This function allows to get the event that is called when the key is released.
+	 *
+	 *	Event parameters:
+	 *	- `SystemKeys pressed_system`: The information about system keys that are pressed now.
+	 *	\param key The key.
+	 *	\return The event binder.
+	 *
+	 *	\~return
+	 *	\brief Клавиша отпущена
+	 *
+	 *	Эта функция позволяет получить событие, вызываемое, когда клавиша отпущена.
+	 *
+	 *	Параметры события:
+	 *	- `SystemKeys pressed_system`: Информация о системных клавишах, нажатых в данный момент.
+	 *	\param key Клавиша.
+	 *	\return «Привязыватель» события.
+	 */
+	EventBinder<SystemKeys /*pressed_system*/>& on_released(EKey key);
+
+	/**
+	 *	\~english
+	 *	\brief The mouse button is pressed
+	 *
+	 *	This function allows to get the event that is called when the mouse button is pressed.
+	 *	\param button The button.
+	 *	\return The event binder.
+	 *
+	 *	\~return
+	 *	\brief Кнопка мыши нажата
+	 *
+	 *	Эта функция позволяет получить событие, вызываемое, когда кнопка мыши нажата.
+	 *	\param button Кнопка.
+	 *	\return «Привязыватель» события.
+	 */
 	EventBinder<>& on_mouse_pressed(EButton button);
+
+	/**
+	 *	\~english
+	 *	\brief The mouse button is released
+	 *
+	 *	This function allows to get the event that is called when the mouse button is released.
+	 *	\param button The button.
+	 *	\return The event binder.
+	 *
+	 *	\~return
+	 *	\brief Кнопка мыши отпущена
+	 *
+	 *	Эта функция позволяет получить событие, вызываемое, когда кнопка мыши отпущена.
+	 *	\param button Кнопка.
+	 *	\return «Привязыватель» события.
+	 */
 	EventBinder<>& on_mouse_released(EButton button);
 
 private:
 	Input();
 
-	std::map<EKey, Event<SystemKeys>> on_pressed_;
-	std::map<EKey, Event<SystemKeys>> on_released_;
+	std::map<EKey, Event<SystemKeys /*pressed_system*/>> on_pressed_;
+	std::map<EKey, Event<SystemKeys /*pressed_system*/>> on_released_;
 	std::map<EButton, Event<>> on_mouse_pressed_;
 	std::map<EButton, Event<>> on_mouse_released_;
 
-	std::map<EKey, EventBinder<SystemKeys>> on_pressed_binder_;
-	std::map<EKey, EventBinder<SystemKeys>> on_released_binder_;
+	std::map<EKey, EventBinder<SystemKeys /*pressed_system*/>> on_pressed_binder_;
+	std::map<EKey, EventBinder<SystemKeys /*pressed_system*/>> on_released_binder_;
 	std::map<EButton, EventBinder<>> on_mouse_pressed_binder_;
 	std::map<EButton, EventBinder<>> on_mouse_released_binder_;
 
--- SnowEngine/Source/Util/Input/Keys.h
+++ SnowEngine/Source/Util/Input/Keys.h
@@ -9,10 +9,23 @@
 //\\         /\\_ \\//\  ___ /\\/ __ /\\  \\/  \//  //  // _//\ __ \//\\  \\  \\//\\//\\//\\//\\//\
 //\_         _\\/__ //\  /\\  \\  \\  \\  \\/  \//  //  __ \//  //  //\\  \\  \\/| SnowEngine |\//\
 //\\__     __/\\//\  /\  /\\  \\_ \\ _\\_ \\ __ // _//  // _//_ //  //\\  \\  \\//| input      |//\
-//\\//_____\//\\____//\__/\\__\\/____/\\/____/\____\//_____\//\_______\\__\\__\\//\| system     |/\
+//\\//_____\//\\____//\__/\\__\\/____/\\/____/\____\//_____\//\____,__\\__\\__\\//\| system     |/\
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\
 
 #pragma once
+
+/**
+ *	\file
+ *	\~english
+ *	\brief The file with keys and buttons list
+ *	
+ *	This file contains enumerations of keyboard keys and mouse buttons.
+ *	
+ *	\~russian
+ *	\brief Файл со списком клавиш и кнопок
+ *	
+ *	Этот файл содержит перечисления клавиш клавиатуры и кнопок мыши.
+ */
 
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Mouse.hpp>
@@ -20,131 +33,164 @@
 namespace snow
 {
 
+/**
+ *	\~english
+ *	\brief Keyboard keys
+ *	
+ *	The enumeration of keyboard keys.
+ *	
+ *	\~russian
+ *	\brief Клавиши клавиатуры
+ *	
+ *	Перечисление клавиш клавиатуры.
+ */
 enum class EKey
 {
-	UNKNOWN = -1,
-	A = 0,
-	B,
-	C,
-	D,
-	E,
-	F,
-	G,
-	H,
-	I,
-	J,
-	K,
-	L,
-	M,
-	N,
-	O,
-	P,
-	Q,
-	R,
-	S,
-	T,
-	U,
-	V,
-	W,
-	X,
-	Y,
-	Z,
-	SPACE,
-	BACKSPACE,
-	ONE,
-	TWO,
-	THREE,
-	FOUR,
-	FIVE,
-	SIX,
-	SEVEN,
-	EIGHT,
-	NINE,
-	ZERO,
-	MINUS,
-	EQUAL,
-	F1,
-	F2,
-	F3,
-	F4,
-	F5,
-	F6,
-	F7,
-	F8,
-	F9,
-	F10,
-	F11,
-	F12,
-	TILDE,
-	ESC,
-	TAB,
-	L_SHIFT,
-	R_SHIFT,
-	L_CTRL,
-	R_CTRL,
-	L_ALT,
-	R_ALT,
-	WIN,
-	ENTER,
-	LEFT,
-	RIGHT,
-	UP,
-	DOWN,
-	L_BRACKET,
-	R_BRACKET,
-	SEMICOLON,
-	QUOTE,
-	BACKSLASH,
-	COMMA,
-	PERIOD,
-	SLASH,
-	HOME,
-	END,
-	PAGE_UP,
-	PAGE_DOWN,
-	DELETE,
-	INSERT,
-	PAUSE,
-	MENU,
-	NUM_ADD,
-	NUM_SUBTRACT,
-	NUM_MULTIPLY,
-	NUM_DIVIDE,
-	NUM_0,
-	NUM_1,
-	NUM_2,
-	NUM_3,
-	NUM_4,
-	NUM_5,
-	NUM_6,
-	NUM_7,
-	NUM_8,
-	NUM_9
+	UNKNOWN = -1,	/// \~english Unknown key \~russian Неизвестная клавиша
+	A = 0,			/// \~english A key \~russian Клавиша A (Ф на русской раскладке)
+	B,				/// \~english B key \~russian Клавиша B (И на русской раскладке)
+	C,				/// \~english C key \~russian Клавиша C (С на русской раскладке)
+	D,				/// \~english D key \~russian Клавиша D (В на русской раскладке)
+	E,				/// \~english E key \~russian Клавиша E (У на русской раскладке)
+	F,				/// \~english F key \~russian Клавиша F (А на русской раскладке)
+	G,				/// \~english G key \~russian Клавиша G (П на русской раскладке)
+	H,				/// \~english H key \~russian Клавиша H (Р на русской раскладке)
+	I,				/// \~english I key \~russian Клавиша I (Ш на русской раскладке)
+	J,				/// \~english J key \~russian Клавиша J (О на русской раскладке)
+	K,				/// \~english K key \~russian Клавиша K (Л на русской раскладке)
+	L,				/// \~english L key \~russian Клавиша L (Д на русской раскладке)
+	M,				/// \~english M key \~russian Клавиша M (М на русской раскладке)
+	N,				/// \~english N key \~russian Клавиша N (Т на русской раскладке)
+	O,				/// \~english O key \~russian Клавиша O (Щ на русской раскладке)
+	P,				/// \~english P key \~russian Клавиша P (З на русской раскладке)
+	Q,				/// \~english Q key \~russian Клавиша Q (Й на русской раскладке)
+	R,				/// \~english R key \~russian Клавиша R (К на русской раскладке)
+	S,				/// \~english S key \~russian Клавиша S (Ы на русской раскладке)
+	T,				/// \~english T key \~russian Клавиша T (Е на русской раскладке)
+	U,				/// \~english U key \~russian Клавиша U (Г на русской раскладке)
+	V,				/// \~english V key \~russian Клавиша V (М на русской раскладке)
+	W,				/// \~english W key \~russian Клавиша W (Ц на русской раскладке)
+	X,				/// \~english X key \~russian Клавиша X (Ч на русской раскладке)
+	Y,				/// \~english Y key \~russian Клавиша Y (Н на русской раскладке)
+	Z,				/// \~english Z key \~russian Клавиша Z (Я на русской раскладке)
+	SPACE,			/// \~english Space key \~russian Клавиша пробела
+	BACKSPACE,		/// \~english Backspace key \~russian Клавиша Backspace
+	ONE,			/// \~english 1 key \~russian Клавиша 1
+	TWO,			/// \~english 2 key \~russian Клавиша 2
+	THREE,			/// \~english 3 key \~russian Клавиша 3
+	FOUR,			/// \~english 4 key \~russian Клавиша 4
+	FIVE,			/// \~english 5 key \~russian Клавиша 5
+	SIX,			/// \~english 6 key \~russian Клавиша 6
+	SEVEN,			/// \~english 7 key \~russian Клавиша 7
+	EIGHT,			/// \~english 8 key \~russian Клавиша 8
+	NINE,			/// \~english 9 key \~russian Клавиша 9
+	ZERO,			/// \~english 0 key \~russian Клавиша 0
+	MINUS,			/// \~english - key \~russian Клавиша -
+	EQUAL,			/// \~english = key \~russian Клавиша =
+	F1,				/// \~english F1 key \~russian Клавиша F1
+	F2,				/// \~english F2 key \~russian Клавиша F2
+	F3,				/// \~english F3 key \~russian Клавиша F3
+	F4,				/// \~english F4 key \~russian Клавиша F4
+	F5,				/// \~english F5 key \~russian Клавиша F5
+	F6,				/// \~english F6 key \~russian Клавиша F6
+	F7,				/// \~english F7 key \~russian Клавиша F7
+	F8,				/// \~english F8 key \~russian Клавиша F8
+	F9,				/// \~english F9 key \~russian Клавиша F9
+	F10,			/// \~english F10 key \~russian Клавиша F10
+	F11,			/// \~english F11 key \~russian Клавиша F11
+	F12,			/// \~english F12 key \~russian Клавиша F12
+	TILDE,			/// \~english ~ key \~russian Клавиша ~ (Ё на русской раскладке)
+	ESC,			/// \~english Escape key \~russian Клавиша Escape
+	TAB,			/// \~english Tabulation key \~russian Клавиша табуляции
+	L_SHIFT,		/// \~english Left shift key \~russian Клавиша левый Shift
+	R_SHIFT,		/// \~english Right shift key \~russian Клавиша правый Shift
+	L_CTRL,			/// \~english Left control key \~russian Клавиша левый Ctrl
+	R_CTRL,			/// \~english Right control key \~russian Клавиша правый Ctrl
+	L_ALT,			/// \~english Left alt key \~russian Клавиша левый Alt
+	R_ALT,			/// \~english Right alt key \~russian Клавиша правый Alt
+	WIN,			/// \~english Windows key \~russian Клавиша Windows
+	ENTER,			/// \~english Enter (Return) key \~russian Клавиша Enter (Return)
+	LEFT,			/// \~english Left arrow \~russian Cтрелка влево
+	RIGHT,			/// \~english Right arrow \~russian Cтрелка право
+	UP,				/// \~english Up arrow \~russian Cтрелка вверх
+	DOWN,			/// \~english Down arrow \~russian Cтрелка вниз
+	L_BRACKET,		/// \~english [ key \~russian Клавиша [ (Х на русской раскладке)
+	R_BRACKET,		/// \~english ] key \~russian Клавиша ] (Ъ на русской раскладке)
+	SEMICOLON,		/// \~english ; key \~russian Клавиша ; (Ж на русской раскладке)
+	QUOTE,			/// \~english ' key \~russian Клавиша ' (Э на русской раскладке)
+	BACKSLASH,		/// \~english \ key \~russian Клавиша \
+	COMMA,			/// \~english , key \~russian Клавиша , (Б на русской раскладке)
+	PERIOD,			/// \~english . key \~russian Клавиша . (Ю на русской раскладке)
+	SLASH,			/// \~english / key \~russian Клавиша / (. на русской раскладке)
+	HOME,			/// \~english Home key \~russian Клавиша Home
+	END,			/// \~english End key \~russian Клавиша End
+	PAGE_UP,		/// \~english Page Up key \~russian Клавиша Page Up 
+	PAGE_DOWN,		/// \~english Page Down key \~russian Клавиша Page Down
+	DELETE,			/// \~english Delete key \~russian Клавиша Delete
+	INSERT,			/// \~english Insert key \~russian Клавиша Insert
+	PAUSE,			/// \~english Pause key \~russian Клавиша Pause
+	MENU,			/// \~english Menu key \~russian Клавиша Menu
+	NUM_ADD,		/// \~english + numpad key \~russian Клавиша + на цифровой клавиатуре
+	NUM_SUBTRACT,	/// \~english - numpad key \~russian Клавиша - на цифровой клавиатуре
+	NUM_MULTIPLY,	/// \~english * numpad key \~russian Клавиша * на цифровой клавиатуре
+	NUM_DIVIDE,		/// \~english / numpad key \~russian Клавиша / на цифровой клавиатуре
+	NUM_0,			/// \~english 0 numpad key \~russian Клавиша 0 на цифровой клавиатуре
+	NUM_1,			/// \~english 1 numpad key \~russian Клавиша 1 на цифровой клавиатуре
+	NUM_2,			/// \~english 2 numpad key \~russian Клавиша 2 на цифровой клавиатуре
+	NUM_3,			/// \~english 3 numpad key \~russian Клавиша 3 на цифровой клавиатуре
+	NUM_4,			/// \~english 4 numpad key \~russian Клавиша 4 на цифровой клавиатуре
+	NUM_5,			/// \~english 5 numpad key \~russian Клавиша 5 на цифровой клавиатуре
+	NUM_6,			/// \~english 6 numpad key \~russian Клавиша 6 на цифровой клавиатуре
+	NUM_7,			/// \~english 7 numpad key \~russian Клавиша 7 на цифровой клавиатуре
+	NUM_8,			/// \~english 8 numpad key \~russian Клавиша 8 на цифровой клавиатуре
+	NUM_9 			/// \~english 9 numpad key \~russian Клавиша 9 на цифровой клавиатуре
 };
 
+/**
+ *	\~english
+ *	\brief System keys
+ *	
+ *	The structure with the information about which system keys are pressed.
+ *	
+ *	\~russian
+ *	\brief Системные клавиши
+ *	
+ *	Структура с информацией о том, какие системы клавиши нажаты.
+ */
 struct SystemKeys
 {
-	bool LeftShift : 1;
-	bool LeftCtrl : 1;
-	bool LeftAlt : 1;
-	bool RightShift : 1;
-	bool RightCtrl : 1;
-	bool RightAlt : 1;
-	bool Win : 1;
+	bool LeftShift : 1;		/// \~english Left Shift \~russian Левый Shift
+	bool LeftCtrl : 1;		/// \~english Left Control \~russian Левый Ctrl
+	bool LeftAlt : 1;		/// \~english Left Alt \~russian Левый Alt
+	bool RightShift : 1;	/// \~english Right Shift \~russian Правый Shift
+	bool RightCtrl : 1;		/// \~english Right Control \~russian Правый Ctrl
+	bool RightAlt : 1;		/// \~english Right Alt \~russian Правый Alt
+	bool Win : 1;			/// \~english Windows \~russian Windows
 };
 
+/**
+ *	\~english
+ *	\brief Mouse button
+ *	
+ *	The enumeration with mouse buttons.
+ *	
+ *	\~russian
+ *	\brief Кнопки мыши
+ *	
+ *	Перечисление с кнопками мыши.
+ */
 enum class EButton
 {
-	LEFT = 0,
-	RIGHT,
-	MIDDLE,
-	EXTRA_1,
-	EXTRA_2
+	LEFT = 0,	/// \~english Left mouse button \~russian Левая кнопка мыши
+	RIGHT,		/// \~english Right mouse button \~russian Правая кнопка мыши
+	MIDDLE,		/// \~english Middle mouse button \~russian Средняя кнопка мыши
+	EXTRA_1,	/// \~english The first extra mouse button \~russian Первая дополнительная кнопка мыши
+	EXTRA_2		/// \~english The second extra mouse button \~russian Вторая дополнительная кнопка мыши
 };
 
-sf::Keyboard::Key key_snow_to_sfml(EKey key);
-EKey key_sfml_to_snow(sf::Keyboard::Key key);
-sf::Mouse::Button button_snow_to_sfml(EButton button);
-EButton button_sfml_to_snow(sf::Mouse::Button button);
+sf::Keyboard::Key key_snow_to_sfml(EKey key) noexcept;
+EKey key_sfml_to_snow(sf::Keyboard::Key key) noexcept;
+sf::Mouse::Button button_snow_to_sfml(EButton button) noexcept;
+EButton button_sfml_to_snow(sf::Mouse::Button button) noexcept;
 
 }
